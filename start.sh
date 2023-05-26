465e4556d406970f12b4aae0aec272c57c780c89
laquiante <33266194+laquiante@users.noreply.github.com>
2023-05-26 11:50:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@
 # ALQ, Hamburg 2023
 clear
 sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/.nvue/api_file.yaml
-exit 0
+read -n 1 -s
 sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/.netplan/hosts.yaml
-
+read -n 1 -s
 if [ -f /home/cumulus/ON-18/.additional.conf ]; then
         . /home/cumulus/ON-18/.additional.conf
 fi
 echo "please use the correct user depending on the selected training, going forward"
-
+read -n 1 -s
 exit 0
