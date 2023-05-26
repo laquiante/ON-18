87ff0e0d760660dd8c48ce331ec1c4326f93ab03
laquiante <33266194+laquiante@users.noreply.github.com>
2023-05-26 11:59:05 +0200

    Update motd.sh

--- inventory/files/motd.sh
+++ inventory/files/motd.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
-
 # schwarz, rot, grün, gelb, blau, magenta, cyan und weiß: 30, 31, 32, 33, 34, 35, 36, 37.
+
+# inspired by: Ali A.
+
+# Discovering and exracting
+DC="Hamburg single-site"
+OS=$(grep -E '^VERSION=' /etc/os-release | cut -d "\"" -f 2)
+UP=$(uptime -p | awk '{for(i=2;i<=NF;i++){printf "%s ", $i}}')
+IPv4_mgmt=$(ip address show eth0 | grep global | awk '{print $2}')
+IPv4_lo=$(ip address show lo | grep global | awk '{print $2}' | head -n 1)
 
 # Banner
 echo -e " \e[0;32m
@@ -22,7 +29,7 @@ echo -e " \e[0;32m
    ##     #######  ##     ##    ##    ######## ########\e[0m
 "
 
-echo -e "  You are working now in: \e[0;32m" DC Site: Hamburg" \e[0m"
+echo -e "  You are working now in: \e[0;32m" DC Site: $DC" \e[0m"
 echo -e "  On device:              \e[0;32m" $HOSTNAME" \e[0m"
 echo -e "  Operating System:       \e[0;34m" $OS" \e[0m"
 echo -e "  Kernel:                 \e[0;34m" $KERNEL" \e[0m"
