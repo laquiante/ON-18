3b2ec1830be0f5e7a55a8ef47c80956c0141c784
laquiante <33266194+laquiante@users.noreply.github.com>
2023-06-01 09:58:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 # ALQ, Hamburg 2023
-clear
+#clear
 echo "start script: running host and api preparation"
 sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/.nvue/api_file.yaml
 sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/.netplan/hosts.yaml
