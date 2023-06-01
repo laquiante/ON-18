73ef5ba9e94e46286138f0f5d12e0d1adba2e158
laquiante <33266194+laquiante@users.noreply.github.com>
2023-06-01 08:59:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,13 @@
 # -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
 # ALQ, Hamburg 2023
-
+clear
+echo "start script: running host and api preparation"
 sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/.nvue/api_file.yaml
-
 sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/.netplan/hosts.yaml
-
+echo "start script: searching for and running additional configuration items" 
 if [ -f /home/cumulus/ON-18/.additional.conf ]; then
         . /home/cumulus/ON-18/.additional.conf
 fi
 echo "please use the correct user depending on the selected training, going forward"
-clear
 exit 0
