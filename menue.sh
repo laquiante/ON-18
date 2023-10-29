75142a403e146b60a9daaca73be29ed172f0d9c9
laquiante <33266194+laquiante@users.noreply.github.com>
2023-10-29 14:53:31 +0100

    Update menue.sh

--- menue.sh
+++ menue.sh
@@ -29,7 +29,7 @@ echo "************************************* end ********************************
 echo
 echo 
 PS3='Please enter your choice: '
-options=("Cleanup" "Practice-01" "Practice-02" "Practice-03" "Practice-04" "Practice-04 New" "Practice-05" "Practice-06" "Practice-07" "Practice-08" "Practice-09" Practice-10" "5th day TS L3" "5th day TS L2" "5th day TS L3" "5th day TS EVPN" "Quit")
+options=("Cleanup" "Practice-01" "Practice-02" "Practice-03" "Practice-04" "Practice-04 New" "Practice-05" "Practice-06" "Practice-07" "Practice-08" "Practice-09" Practice-11" "5th day TS L3" "5th day TS L2" "5th day TS L3" "5th day TS EVPN" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -88,9 +88,9 @@ do
             sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/labs/practice-09/main.yaml
             break
             ;;  
-        "Practice-10")
-            echo "Practice-10"
-            sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/labs/practice-10/main.yaml
+        "Practice-11")
+            echo "Practice-11"
+            sudo ansible-playbook -i /home/cumulus/ON-18/inventory/files/hosts /home/cumulus/ON-18/labs/practice-11/main.yaml
             break
             ;;
         "5th day TS L2")
